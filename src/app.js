5dbc623fe602ec719edf7f0a1f3b978896e3d6a3
olgatar <leyendaorama@gmail.com>
2016-06-09 21:49:43 +0200

    changes

--- src/app.js
+++ src/app.js
@@ -9,8 +9,15 @@ app.get('/', function(req, res){
   res.send("<h1>I am IN love with Treehouse!</h1>");
 });
 
-app.get('/blog', function(req, res){
-  res.send(posts);
+app.get('/blog/:title?', function(req, res){
+  var title = req.params.title;
+  if (title === undefined) {
+    res.status(503);
+    res.send("This page is under construction!");
+  } else {
+    var post = posts[title];
+    res.send(post);
+  }
 });
 
 app.listen(3000, function(){
